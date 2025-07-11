6627df3d00ee5598f3cb6b1ed8ff50f5905669bc
afkhay <mkagarcia@tip.edu.ph>
2025-07-11 18:55:58 +0800

    2nd Commit

--- src/components/sections/Activities.js
+++ src/components/sections/Activities.js
@@ -8,22 +8,85 @@ const ActivitiesSection = () => {
   const activities = {
     Competitions: [
       {
-        title: "Title",
-        org: "Org • Date",
-        desc: "Description",
-        image: "/activitiesphotos/image.png",
-        highlights: ["1", "2", "3"]
+        title: "Inhinyera's Next Top E-Model: Wave 3",
+        org: "T.I.P. Manila • Inhinyera Youth Organization • October 2022",
+        desc: "I represented the department as a first-year student. I created my own advocacy video and confidently participated in the question-and-answer portion, earning a title.",
+        image: "/activitiesphotos/compe1.png",
+        highlights: ["CpE Representative","2nd Placer"]
       },
       {
-        title: "Title",
-        org: "Org • Date",
-        desc: "Description",
-        image: "/activitiesphotos/image.png",
-        highlights: ["1", "2", "3"]
+        title: "61st Founding Anniversary: Festival Dance Competition",
+        org: "T.I.P. Manila • Supreme Student Council (SSC) • February 2023",
+        desc: "I joined the Computer Engineering × Chemical Engineering team in a bangus festival-inspired dance performance, competing against other joint departments.",
+        image: "/activitiesphotos/compe2.png",
+        highlights: ["CpE Representative","4th Placer"]
+      },
+      {
+        title: "Nights of Lights and Carols: Jingle Jam Dance-Off",
+        org: "T.I.P. Manila • Supreme Student Council (SSC) • December 2023",
+        desc: "I represented the Computer Engineering Department as part of the CpElves team. We told a Christmas love story through dance, competing against other departments.",
+        image: "/activitiesphotos/compe3.png",
+        highlights: ["Cpe Representative","3rd Placer"]
+      },
+      {
+        title: "Inhinyera's SABEL: Traditional Poster Making",
+        org: "T.I.P. Manila • Inhinyera Youth Organization • March 2024",
+        desc: "I represented the Computer Engineering Department, together with my teammates, we created a hand-drawn poster that reflected women empowerment in engineering.",
+        image: "/activitiesphotos/compe4.png",
+        highlights: ["CpE Representative","Champion"]
+      },
+      {
+        title: "CEA Days: Mr. and Ms. CEA",
+        org: "T.I.P. Manila • College of Engineering and Architecture (CEA) • April 2024",
+        desc: "Although the main event was cancelled, I took part in rehearsals, photoshoots, and promotions, proudly representing the Computer Engineering Department and supporting unity within the college.",
+        image: "/activitiesphotos/compe5.png",
+        highlights: ["CpE Representative"]
+      },
+      {
+        title: "Student Life Fair: RSO Competitions",
+        org: "T.I.P. Manila • League of Recognized Student Organizations (LORSO) • October 2024",
+        desc: "As the newly appointed Computer Engineering Student Society (CoESS) secretary, I contributed to the creative process of booth design, bulletin board design, movie poster, and advocacy video making.",
+        image: "/activitiesphotos/compe6.png",
+        highlights: ["CoESS Representative"]
+      },
+      {
+        title: "IP Circle League Festival of Stories: The Story",
+        org: "Intellectual Property Office of the Philippines (IPOPHL) • October 2024",
+        desc: "I joined the short film-making segment as scriptwriter and production designer. I helped create a film that highlighted the role of intellectual property in addressing social issues through film.",
+        image: "/activitiesphotos/compe7.png",
+        highlights: ["T.I.P. Manila Representative"]
+      },
+      {
+        title: "ICpEP CpE Challenge: Quiz Bowl",
+        org: "Institute of Computer Engineers of the Philippines (ICpEP) • December 2024",
+        desc: "I competed in the national Quiz Bowl alongside two teammates. We showcased our knowledge in core CpE subjects while connecting with fellow engineering students across the country.",
+        image: "/activitiesphotos/compe8.png",
+        highlights: ["T.I.P. Manila Representative"]
+      },
+      {
+        title: "Inhinyera KARERA: LeadHerAwards",
+        org: "T.I.P. Manila • Inhinyera Youth Organization • March 2025",
+        desc: "I was recognized as a recipient of an award which honored my excellence and leadership as one of the most outstanding female leaders in STEM.",
+        image: "/activitiesphotos/compe9.png",
+        highlights: ["T.I.P. Manila Representative"]
+      },
+      {
+        title: "8th MRSP National Skills Competition: AI in Robotics",
+        org: "Mechatronics and Robotics Society of the Philippines (MRSP) • June 2025",
+        desc: "I and my teammates competed against schools from across the country in building an AI-powered robot that scanned QR codes in sequence to solve a maze, earning a title for our institution.",
+        image: "/activitiesphotos/compe10.png",
+        highlights: ["T.I.P. Manila Representative","2nd Placer"]
       }
     ],
-    Events: [],
-    Charities: [],
+    Events: [
+      {
+        title: "Computer Hardware and Software Troubleshooting Workshop",
+        org: "Computer Engineering Department Student Council • May 2023",
+        desc: "Insert",
+        image: "/activitiesphotos/compe3.png",
+        highlights: ["Participant", "3rd Placer"]
+      }
+    ],
     Projects: [],
   };
 
@@ -32,7 +95,7 @@ const ActivitiesSection = () => {
       <h2 className="activities-title">Activities</h2>
 
       <div className="skills-tab">
-        {["Competitions", "Events", "Charities", "Projects"].map((tab) => (
+        {["Competitions", "Events", "Projects"].map((tab) => (
           <button
             key={tab}
             className={`skills-tab-button ${activeTab === tab ? "active" : ""}`}
